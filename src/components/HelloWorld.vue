e363e658cd2bbc6b3f4f5ab4148d2fe3f0fdd600
diliburong <diliburong@126.com>
2017-11-09 23:16:38 +0800

    test

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -4,7 +4,7 @@
     <h2>Essential Links</h2>
     <ul>
       <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
+      <li><a href="https://forum.vuejs.org" target="_blank">222</a></li>
       <li><a href="https://chat.vuejs.org" target="_blank">Community Chat</a></li>
       <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
       <br>
